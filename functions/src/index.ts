e769b8eabeeb70892bd69de6ddbf817c57d7603a
RickTimmer <ricktimmer98@gmail.com>
2019-05-13 09:58:05 +0100

    Setup express base

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,18 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
+
+/* Express */
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   response.send("Hello from Firebase!");
+// });
+
+export const api = functions.https.onRequest(app);
