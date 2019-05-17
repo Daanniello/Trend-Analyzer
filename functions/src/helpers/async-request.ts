5588e9211072897871e3aae8ade4cc94f643f958
NotYorick <yorickguichelaar@gmail.com>
2019-05-17 11:05:43 +0200

    Missing file added

---
+++ functions/src/helpers/async-request.ts
@@ -0,0 +1,21 @@
+import * as Request from "request";
+
+class AsyncRequest {
+  async get(url: string, options?: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try {
+        Request.get(
+          url,
+          (error: any, response: Request.Response, body: any) => {
+            if (error) reject(error);
+            resolve(body);
+          }
+        ).form(options);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+}
+
+export default AsyncRequest;
