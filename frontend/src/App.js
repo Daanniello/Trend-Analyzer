c3b0cd02fd5b83b1fd5fea70d92d5a56895c3834
RickTimmer <ricktimmer98@gmail.com>
2019-05-20 16:02:10 +0100

    Created the login screen (not secure)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,26 @@ import { Component } from "react";
 import "./App.css";
 
 import Navigation from "./components/navigation/Navigation";
-
+import LoginForm from "./components/login/login-form";
 import GeneralPage from "./pages/GeneralPage";
+
+import Modal from "@material-ui/core/Modal";
+import DialogContent from "@material-ui/core/DialogContent";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    // const loggedIn = window.localStorage.loggedIn;
+    // console.log(loggedIn);
+    window.localStorage.loggedIn = undefined;
+
     this.state = {
       currentPage: 0,
+      pinCode: "",
       pages: [<GeneralPage />, <div />, <div />],
-      lastUpdated: "16-05-2019 15:00"
+      lastUpdated: "16-05-2019 15:00",
+      loggedIn: window.localStorage.loggedIn
     };
 
     this.setPage = newPage => {
@@ -21,7 +30,40 @@ class App extends Component {
       const state = this.state;
       state.currentPage = newPage;
       this.setState(state);
-      console.log(this.state.currentPage);
+    };
+
+    this.addPin = async value => {
+      const state = this.state;
+      state.pinCode += value;
+      this.setState(state);
+      if (state.pinCode.length > 3) {
+        if (await this.checkLogin(state.pinCode)) {
+          state.loggedIn = true;
+        } else {
+          state.pinCode = "";
+        }
+      }
+      this.setState(state);
+    };
+
+    this.removePin = () => {
+      const state = this.state;
+      this.state.pinCode = this.state.pinCode.substring(
+        0,
+        this.state.pinCode.length - 1
+      );
+      this.setState(state);
+    };
+
+    this.checkLogin = async pinCode => {
+      if (pinCode === "1234") {
+        return true;
+      }
+      const form = document.getElementById("login-form");
+      form.classList.add("login-shake");
+      await new Promise(resolve => setTimeout(resolve, 700));
+      form.classList.remove("login-shake");
+      return false;
     };
   }
 
@@ -33,7 +75,21 @@ class App extends Component {
           currentPage={this.state.currentPage}
           lastUpdated={this.state.lastUpdated}
         />
-        <div id="page-content">{this.state.pages[this.state.currentPage]}</div>
+        {this.state.loggedIn === true ? (
+          <div id="page-content">
+            {this.state.pages[this.state.currentPage]}
+          </div>
+        ) : (
+          <Modal open={true}>
+            <DialogContent>
+              <LoginForm
+                addPin={this.addPin}
+                removePin={this.removePin}
+                pinCode={this.state.pinCode}
+              />
+            </DialogContent>
+          </Modal>
+        )}
       </div>
     );
   }
