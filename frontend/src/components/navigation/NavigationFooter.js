97ff5b093bb010eb1987e82f4b119eef7dcd0483
GertJan25 <gertjanboertien@gmail.com>
2019-05-27 09:50:32 +0200

    Frontend Axios api key

--- frontend/src/components/navigation/NavigationFooter.js
+++ frontend/src/components/navigation/NavigationFooter.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./NavigationFooter.css";
+import * as axios from "axios";
+
+import RequestService from "../../services/request-service";
 
 import Typography from "@material-ui/core/Typography";
 import ButtonBase from "@material-ui/core/ButtonBase";
 
+const request = new RequestService();
+
 const NavigationFooter = props => {
+  const performAnalyzeRequest = async () => {
+    axios.defaults.headers = { "x-api-key": "SOME_API_KEY" };
+    const response = await request.post("/ping", {});
+    alert(response);
+  };
+
   return (
     <div id="navigation-footer">
       {/* <div id="navigation-footer-button">
       
     </div> */}
-      <ButtonBase id="navigation-footer-button">Update</ButtonBase>
+      <ButtonBase id="navigation-footer-button" onClick={performAnalyzeRequest}>
+        Update
+      </ButtonBase>
 
       <div id="navigation-footer-timestamp">
         <Typography
@@ -29,5 +42,6 @@ const NavigationFooter = props => {
     </div>
   );
 };
+
 //  + props.timeStamp
 export default NavigationFooter;
