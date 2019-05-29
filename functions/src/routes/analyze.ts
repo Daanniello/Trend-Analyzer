c16c17643a1896cfa99d13e1ae26d8a6ceaad200
RickTimmer <ricktimmer98@gmail.com>
2019-05-29 10:45:15 +0100

    Disabled Analyzing

--- functions/src/routes/analyze.ts
+++ functions/src/routes/analyze.ts
@@ -1,7 +1,11 @@
 import * as express from "express";
 
 import CorporatieNLFetchEngine from "../engine/corporatienl-fetch-engine";
+import AedesFetchEngine from "../engine/aedes-fetch-engine";
+import ArticleService from "../services/database/article-service";
 const CFE = new CorporatieNLFetchEngine();
+const AFE = new AedesFetchEngine();
+const articleService = new ArticleService();
 
 /* Ping Router */
 const router = express.Router();
@@ -9,7 +13,14 @@ const router = express.Router();
 /* Implement endpoints */
 router.post("", (req, res) => {
   (async () => {
-    await CFE.FetchArticles();
+    // console.time("ANALYZING ARTICLES");
+    // console.time("ANALYZING CORPORATIENL");
+    // await CFE.FetchArticles();
+    // console.timeEnd("ANALYZING CORPORATIENL");
+    // console.time("ANALYZING AEDES");
+    // await AFE.FetchArticles();
+    // console.timeEnd("ANALYZING AEDES");
+    // console.timeEnd("ANALYZING ARTICLES");
   })();
   res.send("Analyze");
 });
