1a6f55a5562cbc83f4e913e5ccaf8db7b411145e
RickTimmer <ricktimmer98@gmail.com>
2019-06-11 10:34:55 +0100

    Added a articles.json so the db is not used

--- functions/src/routes/articles.ts
+++ functions/src/routes/articles.ts
@@ -1,16 +1,18 @@
 import * as express from "express";
 
-import ArticleService from "../services/database/article-service";
+// import ArticleService from "../services/database/article-service";
 
 /* Ping Router */
 const router = express.Router();
 
 /* Implement endpoints */
 router.get("", async (req, res) => {
-  console.time("RETRIEVING ARTICLES");
-  const service = new ArticleService();
-  res.send(await service.getAll());
-  console.timeEnd("RETRIEVING ARTICLES");
+  res.send(require("../../articles"));
+
+  // console.time("RETRIEVING ARTICLES");
+  // const service = new ArticleService();
+  // res.send(await service.getAll());
+  // console.timeEnd("RETRIEVING ARTICLES");
 });
 
 module.exports = router;
